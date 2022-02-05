5bb4641910e4b6e8fdf4539b34e41989f695a7b3
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-02-05 21:58:35 +0300

    st:html body- adding an empty Id for results display

--- index.js
+++ index.js
@@ -7,24 +7,16 @@ function myClass(){
     if (correctAnswer1=="HTML"){score++};
     if (correctAnswer2=="JS"){score++};
     if (correctAnswer3=="NO"){score++};
-    d
+    document.write(score)
 };
 
-
-
-
-
-
-
-
-
-// if (score==3){
-//     show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Excellent.".
-// }
-// else if(score==2){
-//     show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Average.".
-// }
-// else{
-//     show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Average.".
-// }
+if (score==3){
+    show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Excellent.".
+}
+else if(score==2){
+    show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Average.".
+}
+else{
+    show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Average.".
+}
 
