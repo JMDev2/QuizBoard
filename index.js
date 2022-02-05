53d0aed28d7aef9d9f3995bb8e43ed5bde4266ff
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-02-05 22:42:01 +0300

    ft-CSS-empty -adding some styling on the ID empty

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ function myClass(){
     var correctAnswer1=document.form.quiz1.value; 
     var correctAnswer2=document.form.quiz2.value; 
     var correctAnswer3=document.form.quiz3.value; 
-    var show=document.getElementById("show")
+    var empty=document.getElementById("empty")
     if (correctAnswer1=="HTML"){score++};
     if (correctAnswer2=="JS"){score++};
     if (correctAnswer3=="NO"){score++};
-    document.write(score)
-};
+    form.style.display="none"
+    empty.textContent=score;
 
 if (score==3){
-    show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Excellent.".
+    empty.textContent="Your Score is " + Math.floor(score/3*100) + "%. Excellent."
 }
 else if(score==2){
-    show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Average.".
+    empty.textContent="Your Score is " + Math.floor(score/3*100) + "%. Average."
 }
 else{
-    show.textContent="Your Score is " + Math.floor(score/3*100) + "%. Average.".
+    empty.textContent="Your Score is " + Math.floor(score/3*100) + "%. Fail."
 }
-
+};
