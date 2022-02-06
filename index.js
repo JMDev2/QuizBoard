2b126d8819b81a6c000481c94079c01cf5cca913
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-02-06 15:06:16 +0300

    ref:js variable- changing some variable names to give  better meaning

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function myClass(){
-    var score=0;
-    var correctAnswer1=document.form.quiz1.value; 
-    var correctAnswer2=document.form.quiz2.value; 
-    var correctAnswer3=document.form.quiz3.value; 
-    var empty=document.getElementById("empty");
+    var score=1;
+    var correctAnswer1=document.form.qst1.value; 
+    var correctAnswer2=document.form.qst2.value; 
+    var correctAnswer3=document.form.qst3.value; 
+    var result=document.getElementById("result");
     if (correctAnswer1=="HTML"){score++};
     if (correctAnswer2=="JS"){score++};
     if (correctAnswer3=="NO"){score++};  
-    empty.textContent=score;
+    result.textContent=score;
 
 if (score==3){
-    empty.textContent="Your Score is " + score/3*100 + "%. Excellent."
+    result.textContent="Your Score is " + Math.floor(score/3*100) + "%"+ ". Excellent."
 }
 else if(score==2){
-    empty.textContent="Your Score is " + score/3*100 + "%. Average."
+    result.textContent="Your Score is " + Math.floor(score/3*100) + "%"+ ". Average."
 }
 else{
-    empty.textContent="Your Score is " + score/3*100 + "%. Fail."
+    result.textContent="Your Score is " + Math.floor(score/3*100) + "%"+ ". Fail."
 }
 };
